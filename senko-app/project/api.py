d3f2f7fdf3de5f81a7ae0769d43578922adf575a
eguchi <eguchi@asial.co.jp>
2018-06-10 16:46:35 +0900

    dev

--- senko-app/project/api.py
+++ senko-app/project/api.py
@@ -5,5 +5,13 @@ app = Flask(__name__)
 def hello():
   return "Hello World!"
 
+@app.route("/hello")
+def hello2():
+  return "23"
+
+@app.route("/user/<username>")
+def show_user_profile(username):
+  return 'User %s' % username
+
 if __name__ == '__main__':
   app.run(host="0.0.0.0", port=5000, debug=True)
