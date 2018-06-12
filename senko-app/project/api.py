3710364279cabcdd989c08de57168c380cca7495
eguchi <eguchi@asial.co.jp>
2018-06-12 14:12:40 +0900

    uwsgi logger

--- senko-app/project/api.py
+++ senko-app/project/api.py
@@ -1,12 +1,12 @@
 # [参考] https://codeburst.io/jwt-authorization-in-flask-c63c1acf4eeb
 from flask import Flask, request
 from flask_restful import Api
+import resources
 
 app = Flask(__name__)
 api = Api(app)
 
-import resources
-api.add_resource(resources.HelloWorld, '/<string:msg>')
+api.add_resource(resources.Applicant, '/applicants')
 
 if __name__ == '__main__':
   app.run(host="0.0.0.0", port=5000, debug=True)
--- senko-app/project/resources.py
+++ senko-app/project/resources.py
@@ -1,5 +1,14 @@
 from flask_restful import Resource
 
-class HelloWorld(Resource):
-    def get(self, msg):
-        return {'msg': 'Hello, %s' % msg}
+"""
+応募者RESTクラス
+"""
+class Applicant(Resource):
+    def get(self):
+        return {'applicant': [1, 2, 3]}, 200
+    def post(self):
+        return {'msg': 'created'}, 201
+    def patch(self):
+        return {'msg': 'modified'}, 200
+    def delete(self):
+        return {'msg': 'no content'}, 204
