0159ecc5e93cd8fdafd11a3d4bb673e00454bfb8
eguchi <eguchi@asial.co.jp>
2018-06-17 22:28:37 +0900

    mysql sqlarchemy 連携

--- senko-app/project/config.py
+++ senko-app/project/config.py
@@ -4,13 +4,13 @@
 import os
 
 class DevelopmentConfig:
-    # Flask
-    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/flask_sample?charset=utf8'.format(**{
-    #     'user': os.getenv('DB_USER', 'root'),
-    #     'password': os.getenv('DB_PASSWORD', ''),
-    #     'host': os.getenv('DB_HOST', 'localhost'),
-    # })
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/{database}?charset=utf8'.format(**{
+        'user': os.getenv('DB_USER', 'root'),
+        'password': os.getenv('DB_PASSWORD', 'password'),
+        'host': os.getenv('DB_HOST', 'localhost'),
+        'database': os.getenv('DB_DATABASE', 'senko'),
+    })
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'some-secret-string'
 Config = DevelopmentConfig
