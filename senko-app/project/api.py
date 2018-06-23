c8c3c66aef7d4677a3981cbac9b77ecf0969138b
eguchi <eguchi@asial.co.jp>
2018-06-23 19:10:27 +0900

    access_token実装

--- senko-app/project/api.py
+++ senko-app/project/api.py
@@ -2,20 +2,17 @@
 from flask import Flask, request
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
-# from flask_jwt_extended import JWTManager
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 
 # JWT設定
-# app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
-# jwt = JWTManager(app)
+app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
+jwt = JWTManager(app)
 
 # DB設定
 app.config.from_object('config.Config')
 db = SQLAlchemy(app)
-
-# after initializing SQLAlchemy add JWT secret key constant and initialize JWT
-# from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt)
 
 @app.before_first_request
 def create_tables():
--- senko-app/project/resources.py
+++ senko-app/project/resources.py
@@ -3,6 +3,7 @@
 """
 from flask_restful import Resource, reqparse
 from models import UserModel
+from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt)
 
 parser = reqparse.RequestParser()
 parser.add_argument('username', help = 'This field cannot be blank', required = True)
@@ -26,13 +27,13 @@ class UserRegistration(Resource):
         try:
             new_user.save_to_db()
 
-            # access_token = create_access_token(identity = data['username'])
-            # refresh_token = create_refresh_token(identity = data['username'])
+            access_token = create_access_token(identity = data['username'])
+            refresh_token = create_refresh_token(identity = data['username'])
 
             return {
-                'message': 'User {} was created'.format(data['username'])
-                # 'access_token': access_token,
-                # 'refresh_token': refresh_token
+                'message': 'User {} was created'.format(data['username']),
+                'access_token': access_token,
+                'refresh_token': refresh_token
             }, 201
         except:
             return {'message': 'Something went wrong'}, 500
@@ -47,17 +48,17 @@ class UserLogin(Resource):
         if not current_user or not UserModel.verify_hash(data['password'], current_user.password):
             return {'message': 'User {} doesn\'t exist'.format(data['username'])}, 401
 
-        # access_token = create_access_token(identity = data['username'])
-        # refresh_token = create_refresh_token(identity = data['username'])
+        access_token = create_access_token(identity = data['username'])
+        refresh_token = create_refresh_token(identity = data['username'])
 
         return {
-            'message': 'Logged in as {}'.format(current_user.username)
-            # 'access_token': access_token,
-            # 'refresh_token': refresh_token
+            'message': 'Logged in as {}'.format(current_user.username),
+            'access_token': access_token,
+            'refresh_token': refresh_token
         }
 
 class AllUsers(Resource):
-    # @jwt_required
+    @jwt_required
     def get(self):
         return UserModel.return_all()
 
@@ -65,15 +66,11 @@ class AllUsers(Resource):
 応募者RESTクラス
 """
 class Applicant(Resource):
-    # @jwt_required
     def get(self):
         return {'applicant': [1, 2, 3]}, 200
-    # @jwt_required
     def post(self):
         return {'msg': 'created'}, 201
-    # @jwt_required
     def patch(self):
         return {'msg': 'modified'}, 200
-    # @jwt_required
     def delete(self):
         return {'msg': 'no content'}, 204
