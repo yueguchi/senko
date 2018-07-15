b85935ce0a31fb73827f1b9a48a7fab563ef003c
eguchi <eguchi@asial.co.jp>
2018-07-15 19:47:45 +0900

    applicantsのvalue追加

--- senko-app/project/models/applicant.py
+++ senko-app/project/models/applicant.py
@@ -45,6 +45,18 @@ class ApplicantModel(db.Model):
     def return_list(cls, limit, page):
         def to_json(x):
             return {
-                'name': x.name
+                'id': x.id,
+                'name': x.name,
+                'sex': x.sex,
+                'email': x.email,
+                'birth': x.birth.isoformat(),
+                'address': x.address,
+                'zip1': x.zip1,
+                'zip2': x.zip2,
+                'final_education': x.final_education,
+                'reason': x.reason,
+                'janome_word': x.janome_word,
+                'created_at': x.created_at.isoformat(),
+                'updated_at': x.updated_at.isoformat()
             }
         return {"applicants": list(map(lambda x: to_json(x), cls.query.order_by(cls.created_at).paginate(page, limit).items))}
